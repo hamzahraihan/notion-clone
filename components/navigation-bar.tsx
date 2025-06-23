77a70495778776f0e1b3b286071b41e3b9f2326b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-23 14:24:37 +0700

    add toggle theme, login button, changes in color background

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { useScrollTop } from "@/hooks/useScrollTop";
+import { Button } from "./ui/button";
+import ModeToggle from "./toggle-theme";
 
 const Navigation = () => {
   const isScrollTop = useScrollTop();
 
   return (
     <div
-      className={`bg-white sticky top-0 dark:text-white flex flex-row p-4 border-b-neutral-100 ${isScrollTop && "border-b-2"}`}
+      className={`bg-white dark:bg-[#1f1f1f] sticky top-0 dark:text-white font-bold flex flex-row items-center justify-between p-8 border-b-neutral-100 ${isScrollTop && "border-b-2"}`}
     >
-      Noteground
+      <p>Noteground</p>
+      <div className="flex items-center justify-center gap-2">
+        <Button>Login</Button>
+        <ModeToggle />
+      </div>
     </div>
   );
 };

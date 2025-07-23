b954072e6f83dc137d2d41997676b069d7474d9f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-23 21:42:33 +0700

    adjust max width for responsiveness

--- app/(documents)/[documentId]/page.tsx
+++ app/(documents)/[documentId]/page.tsx
@@ -6,5 +6,12 @@ export default async function Documents({
   const { documentId } = await params;
   console.log(documentId);
 
-  return <div>hello</div>;
+  return (
+    <div className="py-4 px-2 max-w-ful h-full m-auto ">
+      <div className="w-full max-w-96 m-auto">
+        <h1>hello</h1>
+        <div className="font-bold">IBM Fullstack</div>
+      </div>
+    </div>
+  );
 }

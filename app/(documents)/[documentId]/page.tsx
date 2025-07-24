68db5d50ef2cae095c5b11cdd30bbdbfe8c91588
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-24 14:19:43 +0700

    add editor for document editing

--- app/(documents)/[documentId]/page.tsx
+++ app/(documents)/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "../_components/dynamic-editor";
+
 export default async function Documents({
   params,
 }: {
@@ -7,10 +9,9 @@ export default async function Documents({
   console.log(documentId);
 
   return (
-    <div className="py-4 px-2 max-w-ful h-full m-auto ">
-      <div className="w-full max-w-96 m-auto">
-        <h1>hello</h1>
-        <div className="font-bold">IBM Fullstack</div>
+    <div className="max-w-full py-4 px-2 m-auto">
+      <div className="w-full max-w-9/12 m-auto">
+        <Editor />
       </div>
     </div>
   );
